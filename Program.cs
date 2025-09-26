70392c4c3d13212f34e059ac792129d7fe566d8a
Hellen <cristinyhellen75@gmail.com>
2025-09-26 20:14:27 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fale para o cebolinha falar :");
+string fala = Console.ReadLine();
+string falaCebolinha = fala.Replace("r", "l").Replace("R", "L");
+Console.WriteLine(falaCebolinha);
